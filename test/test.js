2d75b149a6357248adbacf72a465e94ee993c59d
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-07-10 21:19:12 +0300

    DocumentFragment implementation

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
 const test = require('ava')
-const { createElement } = require('..')
+const {
+  createDocumentFragment,
+  createElement,
+  createTextNode,
+} = require('..')
 
 test('localName', t => {
   const node = createElement('div')
@@ -57,4 +61,25 @@ test('defaultValue', t => {
 
   t.is(node.defaultValue, 'qwerty')
   t.is(node.outerHTML, '<input value="qwerty">')
+})
+
+test('createTextNode', t => {
+  const node = createElement('div')
+  const text = createTextNode('Hello world!')
+  node.append(text)
+
+  t.is(text, 'Hello world!')
+  t.is(node.outerHTML, '<div>Hello world!</div>')
+})
+
+test('createDocumentFragment', t => {
+  const node = createElement('div')
+  const child = createElement('br')
+  const fragment = createDocumentFragment()
+  const hr = createElement('hr')
+
+  fragment.append('foo', hr, 'bar')
+  node.append(child, fragment)
+
+  t.is(node.outerHTML, '<div><br>foo<hr>bar</div>')
 })
