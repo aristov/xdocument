c1b3b22bbda88398118df21f5a769f35c2ddb027
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-07-11 10:49:30 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/document')
+/**
+ * @module xdocument
+ * @author Vyacheslav Aristov <vv.aristov@gmail.com>
+ */
+if(typeof document === 'undefined') {
+  // Calling via eval() prevents extraction when using a module bundler
+  module.exports = eval('require("./lib/document")')
+}
+else module.exports = document
